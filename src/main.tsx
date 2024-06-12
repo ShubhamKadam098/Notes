3019b4ce04229aad0d4ebc03678489a8a7211edd
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-13 02:08:49 +0530

    refactor: Update main.tsx to use react-router-dom for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Homepage from "./Pages/Homepage";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Homepage />} />
+    </Route>,
+  ),
+);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
