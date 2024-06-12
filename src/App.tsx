4ae0c2651195442b92911dfa364e034cc3337706
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-13 01:59:49 +0530

    Add initial project files and configurations of typescript, tailwind, shadcn ui, react router.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main className="min-h-screen">
+      <section className="container max-w-screen-2xl mx-auto">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button>Add</Button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
