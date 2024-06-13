5e03ed5a8c51b89f9a4a02dfd53607bc551ad996
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-13 16:27:27 +0530

    feat: Add @radix-ui/react-toast dependency and implement Toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <main className="min-h-screen">
-      <section className="container max-w-screen-2xl mx-auto">
+      <section className="container mx-auto max-w-screen-2xl">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <Button>Add</Button>
       </section>
