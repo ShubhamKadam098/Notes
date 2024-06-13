1de116e365664ee5ca76ce9e3adb3ba86ebb3129
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-13 16:27:38 +0530

    chore: Update Layout component to include main container for consistent page structure

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,9 +5,13 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <main className="min-h-screen">
+        <section className="container mx-auto max-w-screen-2xl">
+          <Header />
+          <Outlet />
+          <Footer />
+        </section>
+      </main>
     </>
   );
 };
