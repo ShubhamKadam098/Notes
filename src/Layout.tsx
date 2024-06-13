bb781c7e77fe007017b81a1b80195b149de8d8a7
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-13 20:55:12 +0530

    chore: Update Layout component to include ThemeProvider and ThemeToggle for theming support

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,17 +1,43 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "./contexts/theme";
+import { useEffect, useState } from "react";
 
 const Layout = () => {
+  let mode;
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    mode = "dark";
+    document.documentElement.classList.add("dark");
+  } else {
+    mode = "light";
+    document.documentElement.classList.remove("dark");
+  }
+  const [themeMode, setThemeMode] = useState(mode);
+  const darkTheme = () => setThemeMode("dark");
+  const lightTheme = () => setThemeMode("light");
+
+  useEffect(() => {
+    document.documentElement.classList.remove("dark", "light");
+    document.documentElement.classList.add(themeMode);
+  }, [themeMode]);
   return (
     <>
-      <main className="min-h-screen">
-        <section className="container mx-auto max-w-screen-2xl">
-          <Header />
-          <Outlet />
-          <Footer />
-        </section>
-      </main>
+      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+        <main className="">
+          <section className="mx-auto flex min-h-screen max-w-screen-2xl flex-col border border-white">
+            <Header />
+            <div className="flex-grow">
+              <Outlet />
+            </div>
+            <Footer />
+          </section>
+        </main>
+      </ThemeProvider>
     </>
   );
 };
