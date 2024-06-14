e5a860a3b03f3a0b00c480e36dfc4927c7a82c7c
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-14 18:38:03 +0530

    chore: Update Header component with MobileSidebar for responsive navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import ThemeToggle from "../Theme/ThemeToggle";
-import { Menu } from "lucide-react";
+import MobileSidebar from "../SideBar/MobileSidebar";
 
 const Header = () => {
   return (
     <header className="border-b border-slate-300 dark:border-slate-600">
-      <nav className="container border-gray-200 bg-light-secondary px-4 py-2.5 dark:bg-dark-secondary lg:px-6">
+      <nav className="border-gray-200 bg-light-secondary px-4 py-2.5 dark:bg-dark-secondary lg:px-6">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-          <Menu className="md:hidden" />
+          <MobileSidebar />
           <a href="https://flowbite.com" className="flex items-center">
             <img
               src="/public/icons/logo.svg"
