1d1b5f1d67661f0671828146663d68203be30b71
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-14 22:36:12 +0530

    refactor: Update search input component and add note button

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,12 +1,25 @@
 import FilterOptions from "@/components/Card/FilterOptions";
 import sampleData from "../../Temp/sampleData";
 import DisplayCard from "@/components/Card/DisplayCard";
-import { SearchInput } from "@/components/SearchInput";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { AddNoteBtn } from "@/components/Note/AddNoteBtn";
 const Homepage = () => {
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <section className="max-h-full min-h-full flex-grow py-4">
       <div className="container">
-        <SearchInput />
+        <div className="mx-auto flex w-full max-w-lg items-center space-x-2 py-4">
+          <Input
+            type="text"
+            placeholder="Search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="shadow-sm focus-visible:ring-transparent"
+          />
+          <AddNoteBtn />
+        </div>
         <FilterOptions />
 
         <DisplayCard NotesList={sampleData} />
--- src/components/Note/AddNoteBtn.tsx
+++ src/components/Note/AddNoteBtn.tsx
@@ -1,18 +1,68 @@
-import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import { Input } from "@/components/ui/input";
 import { SquarePlus } from "lucide-react";
+import Note from "@/Models/NoteModel";
+import { useState } from "react";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
-export function SearchInput() {
+export function AddNoteBtn() {
+  const [notes, setNotes] = useState<Note>({
+    id: "",
+    title: "",
+    content: "",
+    labels: [],
+    color: "",
+    pinned: false,
+    archived: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
   return (
-    <div className="mx-auto flex w-full max-w-lg items-center space-x-2">
-      <Input
-        type="text"
-        placeholder="Search"
-        className="shadow-sm outline-none focus:ring-0"
-      />
-      <Button className="bg-[#f9dd8f] px-6 shadow hover:bg-yellow-400 dark:bg-blue-400 dark:hover:bg-blue-600">
-        <SquarePlus />
-      </Button>
-    </div>
+    <Dialog>
+      <DialogTrigger>
+        <Button className="bg-yellow-400 px-6 shadow hover:bg-yellow-500 dark:bg-blue-400 dark:hover:bg-blue-600">
+          <SquarePlus />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="bg-light-secondary dark:bg-dark-secondary">
+        <DialogHeader>
+          <DialogDescription>
+            <div className="flex flex-col gap-4">
+              <Input
+                placeholder="Title"
+                value={notes.title}
+                onChange={(e) =>
+                  setNotes((prev) => ({ ...prev, title: e.target.value }))
+                }
+                className="rounded-none border-none border-transparent bg-transparent text-lg font-semibold text-black ring-offset-0 placeholder:text-xl placeholder:font-semibold focus:border-b focus:border-white focus-visible:border-transparent focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 focus-visible:ring-offset-transparent dark:text-white"
+              />
+              <Textarea
+                placeholder="Take a note... "
+                value={notes.content}
+                onChange={(e) =>
+                  setNotes((prev) => ({ ...prev, content: e.target.value }))
+                }
+                className="text-md h-fit overflow-y-visible rounded-none border border-none border-white bg-transparent text-slate-900 ring-offset-0 focus:border-b focus:border-white focus-visible:border-transparent focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 focus-visible:ring-offset-transparent dark:text-white"
+              />
+
+              <div className="flex items-center justify-end">
+                <Button className="bg-yellow-400 px-6 shadow hover:bg-yellow-500 dark:bg-blue-400 dark:hover:bg-blue-600">
+                  Add Note
+                </Button>
+              </div>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
