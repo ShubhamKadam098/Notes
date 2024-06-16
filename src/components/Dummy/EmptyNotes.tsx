44432f1ec92b031f241d8f44c9edbbce93a86474
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-16 23:08:17 +0530

    feat: Add EmptyNotes component for displaying a message when there are no notes

---
+++ src/components/Dummy/EmptyNotes.tsx
@@ -0,0 +1,11 @@
+const EmptyNotes = () => {
+  return (
+    <section className="flex h-full items-center justify-center">
+      <h1 className="w-fit text-2xl font-semibold text-slate-500 dark:text-slate-600">
+        Your added notes will appear here
+      </h1>
+    </section>
+  );
+};
+
+export default EmptyNotes;
