f8ebddfb1002e584cb5598803b211e68efffb70d
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-16 23:10:59 +0530

    refactor: Update layout of home page

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -8,20 +8,19 @@ const Homepage = () => {
   const [searchInput, setSearchInput] = useState("");
 
   return (
-    <section className="max-h-full min-h-full flex-grow py-4">
-      <div className="container">
-        <div className="mx-auto flex w-full max-w-lg items-center space-x-2 py-4">
-          <Input
-            type="text"
-            placeholder="Search"
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-            className="shadow-sm focus-visible:ring-transparent"
-          />
-          <AddNoteBtn />
-        </div>
-        <FilterOptions />
-
+    <section className="flex h-full max-h-full flex-grow flex-col px-4 py-4">
+      <div className="mx-auto flex w-full max-w-lg items-center space-x-2 py-4">
+        <Input
+          type="text"
+          placeholder="Search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          className="shadow-sm focus-visible:ring-transparent"
+        />
+        <AddNoteBtn />
+      </div>
+      <FilterOptions />
+      <div className="flex-grow">
         <DisplayCard NotesList={sampleData} />
       </div>
     </section>
