493393cb76a81a918aa8d6c85469778d47abd73d
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-18 01:41:12 +0530

    refactor: Update Homepage to filter and sort notes based on search input and pinning status

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,11 +1,29 @@
 import FilterOptions from "@/components/Card/FilterOptions";
-import sampleData from "../../Temp/sampleData";
 import DisplayCard from "@/components/Card/DisplayCard";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { AddNoteBtn } from "@/components/Note/AddNoteBtn";
+import NotesContext from "@/contexts/NotesContext";
+
 const Homepage = () => {
+  const { notes } = useContext(NotesContext);
   const [searchInput, setSearchInput] = useState("");
+
+  const filteredNotes = notes
+    .filter((note) =>
+      note.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    .sort((a, b) => {
+      if (a.pinned && !b.pinned) {
+        return -1;
+      } else if (!a.pinned && b.pinned) {
+        return 1;
+      } else {
+        return (
+          new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+        );
+      }
+    });
 
   return (
     <section className="flex h-full max-h-full flex-grow flex-col px-4 py-4">
@@ -21,9 +39,10 @@ const Homepage = () => {
       </div>
       <FilterOptions />
       <div className="flex-grow">
-        <DisplayCard NotesList={sampleData} />
+        <DisplayCard NotesList={filteredNotes} />
       </div>
     </section>
   );
 };
+
 export default Homepage;
