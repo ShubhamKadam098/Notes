317a6bcb91abbd5bf6cd38b465b2e1655216c543
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-17 16:51:23 +0530

    Added component to display message of upcomming features

---
+++ src/components/Dummy/ComminSoon.tsx
@@ -0,0 +1,9 @@
+const ComminSoon = () => {
+  return (
+    <div className="m-auto flex h-screen w-auto items-center justify-center">
+      <h1 className="text-2xl">Comming Soon ⏰...</h1>
+    </div>
+  );
+};
+
+export default ComminSoon;
