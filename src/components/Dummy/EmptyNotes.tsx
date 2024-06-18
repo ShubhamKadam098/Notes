d9b358a4eedf41a710f6aed712787eb5c5aff674
Shubham Kadam <kadamshubham098@gmail.com>
2024-06-18 19:04:12 +0530

    refactor: Update EmptyNotes component to use consistent text color

--- src/components/Dummy/EmptyNotes.tsx
+++ src/components/Dummy/EmptyNotes.tsx
@@ -1,7 +1,7 @@
 const EmptyNotes = () => {
   return (
     <section className="flex h-full items-center justify-center">
-      <h1 className="w-fit text-2xl font-semibold text-slate-500 dark:text-slate-600">
+      <h1 className="w-fit text-2xl font-semibold text-slate-400 dark:text-slate-600">
         Your added notes will appear here
       </h1>
     </section>
